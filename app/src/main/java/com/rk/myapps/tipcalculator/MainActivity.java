9a3805c11951dd305d4cdbd3f06ee869797049c9
riddhik84 <riddhik84@gmail.com>
2016-08-24 17:31:03 +0530

    Version 1.0 - Added AdMob Integration

--- app/src/main/java/com/rk/myapps/tipcalculator/MainActivity.java
+++ app/src/main/java/com/rk/myapps/tipcalculator/MainActivity.java
@@ -14,6 +14,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
@@ -70,6 +73,15 @@ public class MainActivity extends AppCompatActivity {
 //
 //            }
 //        });
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
     }
 
     @Override
